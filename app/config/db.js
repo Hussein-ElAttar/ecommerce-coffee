172c5c0e3b36246107bd031b6b8ac305d01672ee
Hussein-ElAttar <hussien.elattar1@gmail.com>
2020-07-25 23:30:59 +0200

    Update mongodb config

--- app/config/db.js
+++ app/config/db.js
@@ -1,5 +1,11 @@
-const DEFAULT_DB_URI = "mongodb://localhost:27017/ecommerce";
+const DEFAULT_DB_URI = "mongodb://mongo:27017/ecommerce";
+const MONGO_DB_CONNECTION_OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
 
 module.exports = {
   MONGO_DB_URI: process.env.MONGO_DB_URI || DEFAULT_DB_URI,
+  MONGO_DB_CONNECTION_OPTIONS
 };
--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 // packages
 const express = require('express')
 const mongoose = require('mongoose');
-const {MONGO_DB_URI} = require('./config/db');
+const {MONGO_DB_URI, MONGO_DB_CONNECTION_OPTIONS} = require('./config/db');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
-mongoose.connect(MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(MONGO_DB_URI, MONGO_DB_CONNECTION_OPTIONS);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
