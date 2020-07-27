f89abfe8dee940414e4af39a523aba8025357f3b
Hussein-ElAttar <hussien.elattar1@gmail.com>
2020-07-27 03:51:07 +0200

    Add product controller

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -1,3 +1,24 @@
+const Product = require("../models/product");
+const Joi = require("joi");
+const responseHandler = require("../helpers/responseHandler");
+const Category = require("../models/category");
+
 exports.index = async (req, res) => {
-  res.json("products route");
+  const filters = req.query.filters;
+  const categoryName = req.query.categoryName;
+  let queryFilters = {};
+
+  if (categoryName) {
+    const category = await Category.findByName(categoryName);
+    queryFilters["category"] = category;
+  }
+
+  if (filters) {
+    for (const [attributeName, value] of Object.entries(filters)) {
+      queryFilters[`attributes.${attributeName}`] = value;
+    }
+  }
+
+  const products = await Product.find(queryFilters);
+  return res.json(responseHandler.getSuccessResponse(products));
 };
