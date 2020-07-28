e19c02bcf98796d3d12bdce1def2e770360ff242
Hussein-ElAttar <hussien.elattar1@gmail.com>
2020-07-28 23:05:37 +0200

    Move controllers

--- app/controllers/v1/productController.js
+++ app/controllers/v1/productController.js
@@ -1,9 +1,9 @@
-const Product = require("../models/product");
+const Product = require("../../models/product");
 const Joi = require("joi");
-const responseHandler = require("../helpers/responseHandler");
-const productService = require("../services/productService");
-const { IndexValidator } = require("../validators/productValidator");
-const { VALIDATION_ERROR } = require("../constants/errors");
+const responseHandler = require("../../helpers/responseHandler");
+const productService = require("../../services/productService");
+const { IndexValidator } = require("../../validators/productValidator");
+const { VALIDATION_ERROR } = require("../../constants/errors");
 
 exports.index = async (req, res) => {
   const { error } = Joi.validate(req.query, IndexValidator);
